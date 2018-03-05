fc9cd4bc76c6566103d74e0bf6b56e40cdb4d120
Julien Poulton <julien.poulton@gmail.com>
2018-03-05 14:44:43 +0100

    fixed animation

--- src/main/java/com/codingame/game/Referee.java
+++ src/main/java/com/codingame/game/Referee.java
@@ -191,6 +191,11 @@ public class Referee extends AbstractReferee {
                 }
             }
         }
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                smallGrids[row][col].animate();
+            }
+        }
         Collections.shuffle(validActions, random);
         return validActions;
     }
